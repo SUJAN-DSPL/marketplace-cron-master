a285ad71ee9d8fc4d4940bb733e974b6ead93dc7
nikitha kanakam <nikitha.kanakam@comfortclick.co.uk>
2023-11-29 13:58:53 +0530

    created command ron run custom cornjob

---
+++ app/Console/Commands/CronJobRunCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class CronJobRunCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cronjob:run {name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $name = $this->argument('name');
+        $className = "App\\Jobs\\$name"; // Construct the full class name
+        
+        if (class_exists($className)) {
+            $this->info("Cronjob {$name} is running");
+            $cronJob = new $className(); // Instantiate the class
+            $cronJob->execute(); // Call the execute method
+        } else {
+           return $this->error("Cronjob $name not found.");
+        }
+
+        $this->info("Cronjob {$name} assigned task has been completed");
+    }
+}
