3072b06c3b348971fe5d8451af33a7e65594fafe
nikitha kanakam <nikitha.kanakam@comfortclick.co.uk>
2023-12-05 16:19:45 +0530

    added new changes

--- app/Jobs/ReExecuteFailedRefundEventList.php
+++ app/Jobs/ReExecuteFailedRefundEventList.php
@@ -19,22 +19,7 @@ class ReExecuteFailedRefundEventList extends CronJob
 	public function execute()
 	{
 		FailedRefundEventList::query()->take(self::MAX_ORDER)->each(function ($list) {
-
-			$financialEvent = (new FinanceApiService())
-				->getFinancesByOrderId("206-3183805-6934746")["RefundEventList"][0];
-
-			$newArray["amazon_order_id"] = $financialEvent['AmazonOrderId'];
-			$newArray['seller_order_id'] = $financialEvent['SellerOrderId'];
-			$newArray['marketplace_name'] = $financialEvent['MarketplaceName'];
-			$newArray['posted_date'] = Carbon::parse($financialEvent['PostedDate']);
-			$newArray['shipment_item_adjustment_list'] = $financialEvent['ShipmentItemAdjustmentList'];
-			$newArray['created_at'] = Carbon::now();
-			$newArray['updated_at'] = Carbon::now();
-
-			$refundEventList = RefundEventList::create($newArray);
-
-			(new BackendOrderUpdateService())->syncRefundDataToBackend($refundEventList);
-			$refundEventList->delete();
+			
 		});
 	}
 }
